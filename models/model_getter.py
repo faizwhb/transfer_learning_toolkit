2c60ab5f360ef301331de11bbe46b00ada871061
Faiz Ul Wahab <faiz@nyris.io>
2021-01-07 21:20:48 +0100

    cleanup for models

--- models/model_getter.py
+++ models/model_getter.py
@@ -1,5 +1,5 @@
 import torch
-from models.mobilenet_v2 import make as make_model
+from models.models import make as make_model
 
 def get_model_for_dml(name, num_classes):
     return make_model(name, num_classes)
