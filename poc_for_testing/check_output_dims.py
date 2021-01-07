40a020b1e2437e368f467db5c18c63086ad14f01
Faiz Ul Wahab <faiz@nyris.io>
2021-01-07 21:22:18 +0100

    added poc directory for testing and sanity checks

---
+++ poc_for_testing/check_output_dims.py
@@ -0,0 +1,6 @@
+from models.models import make
+
+my_densenet = make(model_name='densenet', num_classes=10)
+print(my_densenet)
+my_mobilenet = make(model_name='mobilenet', num_classes=10)
+print(my_mobilenet)
